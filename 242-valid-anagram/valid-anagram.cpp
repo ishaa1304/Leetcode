19812156a7acc421a45316871c9f138fae60505d
Isha Deo <76875905+ishaa1304@users.noreply.github.com>
2025-08-30 17:39:05 +0530

    Time: 7 ms (20.49%) | Memory: 9.6 MB (74.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,16 +4,17 @@ public:
         if(s.length() != t.length()){
             return false;
         }
+
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
-        for(int i =0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             if(s[i] != t[i]){
                 return false;
             }
+
         }
         return true;
-        
-        
+
     }
 };
